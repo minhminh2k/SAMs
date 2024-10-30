a7cea2e0b803e76144dd23faa20036b9f7082d9e
minhminh2k <minhqd9112003@gmail.com>
2024-10-30 21:41:28 +0700

    push

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    description="Describe Your Cool Project",
+    description="Segment Anything Model",
     author="",
     author_email="",
     url="https://github.com/user/project",
